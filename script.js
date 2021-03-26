954f100c7f00d1ddcedb2f73b610608bdd47c1e6
0CEAN717 <jakubk7717@gmail.com>
2021-03-26 14:51:33 +0100

    Wykonanie zadania 2

--- script.js
+++ script.js
@@ -22,10 +22,48 @@
 //     console.log(el);
 // }
 
-const prices = document.getElementsByClassName('price')
 
-for (price of prices) {
-    if (price.innerText === '$15') {
-        price.innerText = "$20"
-    }
+
+// 1 Zadanie. Zmienić 15$ na 20$ w box'ie z tytułem "Pro"
+const prices = document.getElementsByClassName('price');
+
+// for (price of prices) {
+// if (price.innerText === '$15') {
+//         price.innerText = "$20"
+//     }
+// }
+
+const callbackTest = function (test, callback) {
+    const blach = document.querySelector(test);
+    callback(blach)
 }
+
+callbackTest('.display-4', function(blach) {
+    blach.style.color = '#fff';
+})
+
+// 2 Zadanie. Zmienić kolor po najechaniu kursorem "Pircing"
+const title = document.querySelector('.display-4');
+
+title.addEventListener('mouseenter', function () {
+    console.log(this)
+    this.style = 'color: blue'
+});
+
+title.addEventListener('mouseleave', function() {
+    title.style = 'color: black'
+});
+
+// 3 Zadanie. Box cenowy "Free", po naciśnięciu przycisku, za pomocą "prompt()" poprosić o podanie imienia i wyświetlić w konsoli.
+const btn = document.getElementById('free');
+
+const titleText = document.getElementsByClassName('my-0 mr-md-auto font-weight-normal');
+    console.log(titleText);
+
+btn.addEventListener('click', function() {
+    let text = prompt('Pleas enter your name');
+    
+    const titleText = document.getElementsByClassName('my-0 mr-md-auto font-weight-normal');
+});
+    
+
